d6e10ceb3fd6318527089e32e69da4802e036bc8
morgs32 <morgan.intrator@gmail.com>
2015-05-12 13:23:05 -0500

    first pull request ever coming

--- js/main.js
+++ js/main.js
@@ -19,54 +19,68 @@ $(document).ready(function(){
 
 /* portfolio mixitup */
 
-	$(window).load(function(){
-    var $container = $('.grid-wrapper');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
+// 	$(window).load(function(){
+//     var $container = $('.grid-wrapper');
+//     $container.isotope({
+//         filter: '*',
+//         animationOptions: {
+//             duration: 750,
+//             easing: 'linear',
+//             queue: false
+//         }
+//     });
  
-    $('.grid-controls li a').click(function(){
-        $('.grid-controls .current').removeClass('current');
-        $(this).addClass('current');
+//     $('.grid-controls li a').click(function(){
+//         $('.grid-controls .current').removeClass('current');
+//         $(this).addClass('current');
  
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    });
-});
+//         var selector = $(this).attr('data-filter');
+//         $container.isotope({
+//             filter: selector,
+//             animationOptions: {
+//                 duration: 750,
+//                 easing: 'linear',
+//                 queue: false
+//             }
+//          });
+//          return false;
+//     });
+// });
 
 
 /* Magnific Popup */
-$('.grid-wrapper').magnificPopup({
-		  delegate: 'a', 
-		  type: 'image',
-		  gallery:{
-			enabled:true
-		  }
-		});
+$('.portfolio').magnificPopup({
+    delegate: 'a', 
+    image: {
+        markup: '<div class="mfp-figure">'+
+                '<div class="mfp-close"></div>'+
+                '<div class="mfp-img"></div>'+ // Floated left
+                '<div class="mfp-title"></div>'+ // This is floated right shows up on the right side
+                '<div class="mfp-bottom-bar">'+
+                  '<div class="mfp-counter"></div>'+
+                '</div>'+
+              '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+
+        cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor. 
+        tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+    },
+
+    gallery:{
+        enabled:true
+    }
+});
 
 
-    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-
-        fixedContentPos: false
-    });
+// $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+//     disableOn: 700,
+//     type: 'iframe',
+//     mainClass: 'mfp-fade',
+//     removalDelay: 160,
+//     preloader: false,
+//     title: 'title',
+//     verticalFit: true,
+//     fixedContentPos: false
+// });
 
 /* Sticky menu */
 $(".navbar").sticky({topSpacing: 0});
@@ -84,19 +98,19 @@ $(".navbar").sticky({topSpacing: 0});
 
 /* Charts*/
     
-$('.chart').waypoint(function() {
-    $(this).easyPieChart({
-    	   barColor: '#3498db',
-    	   size: '150',
-			easing: 'easeOutBounce',
-			onStep: function(from, to, percent) {
-				$(this.el).find('.percent').text(Math.round(percent));
-			}
-	 });
-}, {
-  triggerOnce: true,
-  offset: 'bottom-in-view'
-});
+// $('.chart').waypoint(function() {
+//     $(this).easyPieChart({
+//     	   barColor: '#3498db',
+//     	   size: '150',
+// 			easing: 'easeOutBounce',
+// 			onStep: function(from, to, percent) {
+// 				$(this.el).find('.percent').text(Math.round(percent));
+// 			}
+// 	 });
+// }, {
+//   triggerOnce: true,
+//   offset: 'bottom-in-view'
+// });
 
 
 /* VEGAS Home Slider */
@@ -120,29 +134,29 @@ $('.chart').waypoint(function() {
 
 
 /*Contact form */
-      $('#contact-form').validate({
-        rules: {
-            name: {
-                minlength: 2,
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            message: {
-                minlength: 2,
-                required: true
-            }
-        },
-        highlight: function (element) {
-            $(element).closest('.control-group').removeClass('success').addClass('error');
-        },
-        success: function (element) {
-            element.text('OK!').addClass('valid')
-                .closest('.control-group').removeClass('error').addClass('success');
-        }
-    }); 
+    //   $('#contact-form').validate({
+    //     rules: {
+    //         name: {
+    //             minlength: 2,
+    //             required: true
+    //         },
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         },
+    //         message: {
+    //             minlength: 2,
+    //             required: true
+    //         }
+    //     },
+    //     highlight: function (element) {
+    //         $(element).closest('.control-group').removeClass('success').addClass('error');
+    //     },
+    //     success: function (element) {
+    //         element.text('OK!').addClass('valid')
+    //             .closest('.control-group').removeClass('error').addClass('success');
+    //     }
+    // }); 
 
 
 
